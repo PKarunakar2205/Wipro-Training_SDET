2fd237d1633a3f2aebd62cf8a05545ca53c45ccd
karun <karun@BOOM>
2026-04-20 15:31:55 +0530

    First java Code

---
+++ Wipro_Java_Selenium/src/First_Package/Firstclass.java
@@ -0,0 +1,11 @@
+package First_Package;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Team");
+		// TODO Auto-generated method stub
+
+	}
+
+}
